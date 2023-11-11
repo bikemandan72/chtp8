39e7df0cbc4782cb2b8873881b0d880536da90c2
Denis Anisimov <bikemandan@yandex.ru>
2023-11-11 13:08:02 +0500

    Modified for printing all of the argeuments supplied through command line to the program and stringize symbol #

--- ch13/ex13_08.c
+++ ch13/ex13_08.c
@@ -4,10 +4,12 @@ Write a program that defines and uses macro PRINT to print a string value.
 */
 #include <stdio.h>
 
-#define PRINT(x) puts(x)
+#define PRINT(x) printf(#x"=%s\n", x)
 
 int main(int argc, char *argv[])
 {
-	PRINT(argv[0]);
+	for (unsigned int i = 0; i < argc; ++i) {
+		PRINT(argv[i]);
+	}
     return 0;
 }
